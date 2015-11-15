fb1cbc21562ebe0621ccc3850844db27a769cf84
Giovanni Carvelli <giovannircarvelli@gmail.com>
2015-11-14 19:42:03 -0500

    forgive me father for I have sinned

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import json
 with open('subreddits.txt') as f:
     
     for sub in [line.strip() for line in f if not line.isspace()]:
-    	link = 'https://www.reddit.com/r/' + sub + '.json'
-    	print(link)
+        link = 'https://www.reddit.com/r/' + sub + '.json'
+        print(link)
 
-    	headers = {'User-Agent': 'this is a user agent'}
-    	request = requests.get(link, headers=headers)
+        headers = {'User-Agent': 'this is a user agent'}
+        request = requests.get(link, headers=headers)
 
-    	page = json.loads(request.text)
-    	for key in page.keys():
-    		print(key)
+        page = json.loads(request.text)
+        for key in page.keys():
+            print(key)
 
-    	print(page['kind'])
-    	
+        print(page['kind'])
+        
